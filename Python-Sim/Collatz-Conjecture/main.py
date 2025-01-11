834f87855420c6720001ecf7460173ce335b7f5b
Kasun Nimantha <35260660+nimanthakasun@users.noreply.github.com>
2025-01-11 09:08:44 +0530

    Adds Iterations calculation and plot

--- Python-Sim/Collatz-Conjecture/main.py
+++ Python-Sim/Collatz-Conjecture/main.py
@@ -2,24 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    input_val = 53
-    result_array = np.array([input_val])
-    
-    print(result_array)
-    while input_val > 1:
-        if input_val % 2 ==0:
-            input_val = input_val // 2
-        else:
-            input_val = 3 * input_val +1
+    hist_array = np.array([1])
+    cc_upperbound = 200
 
-        result_array = np.append(result_array,[input_val])
+    for input_val in range(1,cc_upperbound):
+        # input_val = 5528
+        result_array = np.array([input_val])
+        # print(result_array)
+        while input_val > 1:
+            if input_val % 2 ==0:
+                input_val = input_val // 2
+            else:
+                input_val = 3 * input_val +1
 
-    print(result_array)
+            result_array = np.append(result_array,[input_val])
+        hist_array = np.append(hist_array,[result_array.size])
 
-    plt.title("Iteration")
+    # print(result_array)
+    print(hist_array.size)
+
+    # Line graph
+    # plt.title("Iteration")
+    # plt.xlabel("X axis")
+    # plt.ylabel("Y axis")
+    # x = np.arange(0, result_array.size)
+    # plt.plot(x, result_array, color="red")
+
+    # Histogram
+    plt.title("Iteration Histogram")
     plt.xlabel("X axis")
     plt.ylabel("Y axis")
-    x = np.arange(0, result_array.size)
-    plt.plot(x, result_array, color="red")
+    # x = np.arange(0, hist_array.size)
+    plt.bar(range(0,hist_array.size), hist_array)
 
     plt.show()
